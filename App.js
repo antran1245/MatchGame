eacf4f12d47aa3eed0f9624051e3d16bd0ea37b3
antran1245 <antran1245@gmail.com>
2023-03-08 19:40:07 -0800

    update to the finding the definition

--- App.js
+++ App.js
@@ -6,32 +6,46 @@ import WordsList from './components/WordsList';
 export default function App() {
   const [words, setWords] = useState([])
   const [definition, setDefinition] = useState([])
+
   useEffect (() => {
-    fetch("https://random-word-api.herokuapp.com/word?number=10")
-    .then(resp => resp.json())
-    .then(data => {
-      console.log(data)
-      setWords(data)
-    })
-    .catch(err => console.error(err))
+    let findWords = () => {
+      fetch("https://random-word-api.herokuapp.com/word?number=5")
+      .then(resp => resp.json())
+      .then(data => {
+        setWords(data)
+      })
+      .catch(err => console.error(err))
+    }
+    if(words !== 0) {
+      findWords()
+    }
   }, [])
   
   useEffect(() => {
-    let findDefinition = () => {
+    let findDefinition = async() => {
       let temp = []
-      for(const word of words) {
-        fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
-        .then(resp => resp.json())
-        .then(data => {
-          temp.push(data)
-        })
+      for(const item of words) {
+        await new Promise(resolve => setTimeout(resolve, 1000))
+        const resp = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${item}`)
+        const data = await resp.json()
+        if(data.length > 0) {
+          temp.push(data[0])
+        } else {
+          console.log(item, data)
+        }
       }
       setDefinition([...temp])
     }
+    findDefinition()
   }, [words])
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app! {definition.length}</Text>
+      {definition.map((item, index) => {
+        return <View key={index}>
+          <Text>{item.word}</Text>
+        </View>
+      })}
       <WordsList words={words}/>
       <StatusBar style="auto" />
     </View>
