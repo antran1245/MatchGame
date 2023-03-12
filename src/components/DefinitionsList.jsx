0ebfd69c16218fd0d28f4231b03541b41253b815
antran1245 <antran1245@gmail.com>
2023-03-12 00:51:47 -0800

    setting the scrollview to be enable by ontouch of drag word

--- src/components/DefinitionsList.jsx
+++ src/components/DefinitionsList.jsx
@@ -1,8 +1,11 @@
+import { useContext } from 'react';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import ContextProvider from '../context/ContextProvider';
 
 export default function DefinitionsList({ definition } : props) {
+  const state = useContext(ContextProvider)
   return(
-    <ScrollView style={styles.container}>
+    <ScrollView style={styles.container} scrollEnabled={state.scrollable}>
       {definition.map((item, index) => {
         return <View style={styles.box} key={index}>
           <Text>{item.word} {item.meanings[0].definitions[0].definition}</Text>
--- src/components/DraggableWord.jsx
+++ src/components/DraggableWord.jsx
@@ -1,7 +1,10 @@
-import { useRef } from "react"
+import { useContext, useRef } from "react"
 import { Animated, PanResponder, StyleSheet, View, Text } from "react-native"
+import ContextProvider from "../context/ContextProvider"
 
 export default function DraggableWord({word} : props) {
+  const state = useContext(ContextProvider)
+
   const pan = useRef(new Animated.ValueXY()).current
   const panResponder = useRef(
     PanResponder.create({
@@ -12,10 +15,11 @@ export default function DraggableWord({word} : props) {
       }
     })
   ).current
+  
   return(
     <Animated.View
       style={{transform: [{translateX: pan.x}, {translateY: pan.y}]}}
-      {...panResponder.panHandlers} onTouchStart={() => console.log('here')}>
+      {...panResponder.panHandlers} onTouchStart={() => state.setScrollable(false)} onTouchEnd={() => state.setScrollable(true)}>
       <View style={styles.word_container}>
         <Text>{word}</Text>
       </View>
